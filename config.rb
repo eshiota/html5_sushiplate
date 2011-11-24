356df82dcb99f60bd7b6ab23630e60cbfa218250
Eduardo Shiota Yasuda <contato@eduardosy.com.br>
2011-11-24 18:56:39 -0200

    Adding fonts dir to sushiplate

--- config.rb
+++ config.rb
@@ -6,6 +6,7 @@ css_dir = "stylesheets"
 sass_dir = "stylesheets/scss"
 images_dir = "images"
 javascripts_dir = "javascripts"
+fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
